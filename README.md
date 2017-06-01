bb8d55df441b8510bbbde51bab3d9547f3090ecf
mgo-tec <mgo.tec@gmail.com>
2017-06-01 19:35:43 +0900

    New Release
    New Release Beta ver1.0

---
+++ README.md
@@ -0,0 +1,31 @@
+# ESP32_SD_ShinonomeFNT
+This is the Arduino core for the ESP32 library for reading Shinonome 16x16 Japanese font.  
+  
+Beta ver1.0  
+  
+これはShift_JISコードからフリーの東雲フォント(16x16)を ESP-WROOM-32( ESP32 )で読み込むためのライブラリです。  
+SPI ( VSPI )接続の micro SD カードからの読み込み専用です。  
+UTF8toSjisライブラリをインクルードしているので、１行の関数のみでUTF-8文字列から16x16ドットの日本語漢字に変換できます。  
+  
+予め以下のファイルをSDカードにコピーしておく必要があります。  
+（fontというフォルダを作成しておくと良いです。）  
+※ファイル名は半角英数字8文字、拡張子3文字  
+  
+Utf8Sjis.tbl (UTF-8 Shift_JIS変換テーブル)  
+shnmk16.bdf (全角東雲フォント)  
+shnm8x16.bdf (半角東雲フォント, ファイル名の'r'をカットしたもの)  
+  
+【更新履歴】  
+(1.0)  
+ESP32 用リリース  
+  
+Licence:  
+  
+  MITライセンス　　
+  Copyright (c) 2017 Mgo-tec  
+  
+  東雲フォントは/efont/さんが保守開発を行い、ライセンスはPublic Domainです。  
+  
+詳しくはブログ参照：  
+  
+https://www.mgo-tec.com  
---
+++ src/ESP32_SD_ShinonomeFNT.h
@@ -0,0 +1,70 @@
+/*
+  ESP32_SD_ShinonomeFNT.h - Arduino core for the ESP32 Library.
+  Beta version 1.0
+  This is micro SD card library for reading Shinonome font.  
+  
+The MIT License (MIT)
+
+Copyright (c) 2017 Mgo-tec
+Blog URL ---> https://www.mgo-tec.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+Licence of Shinonome Font is Public Domain.
+Maintenance development of Font is /efont/.
+*/
+
+#ifndef _ESP32_SD_SHINONOMEFNT_H_INCLUDED
+#define _ESP32_SD_SHINONOMEFNT_H_INCLUDED
+
+#include <Arduino.h>
+#include "ESP32_SD_UTF8toSJIS.h"
+#include <SD.h>
+#include <SPI.h>
+
+class ESP32_SD_ShinonomeFNT
+{
+public:
+  ESP32_SD_ShinonomeFNT(uint8_t cs, uint32_t sd_freq);
+
+  void SD_Shinonome_Init3F(const char* UTF8SJIS_file, const char* Shino_Half_Font_file, const char* Shino_Zen_Font_file);
+  void SD_Shinonome_Init2F(const char* Shino_Half_Font_file, const char* Shino_Zen_Font_file);
+  void SD_Shinonome_Close3F();
+  void SD_Shinonome_Close2F();
+  uint16_t StrDirect_ShinoFNT_readALL(String str, uint8_t font_buf[][16]);
+  uint16_t SjisShinonomeFNTread_ALL(String str, uint8_t* sj_code, uint8_t font_buf[][16]);
+  uint8_t SjisToShinonome16FontRead(File f1, File f2, uint8_t Direction, int16_t Angle, uint8_t jisH, uint8_t jisL, uint8_t buf1[16], uint8_t buf2[16]);
+  uint8_t SjisToShinonome16FontRead2(uint8_t jisH, uint8_t jisL, uint8_t buf1[16], uint8_t buf2[16]);
+  void SjisToShinonome16FontRead_ALL(File f1, File f2, uint8_t Direction, int16_t Angle, uint8_t* Sjis, uint16_t sj_length, uint8_t font_buf[][16]);
+  void Scroller_RtoL(uint8_t disp_char_max, uint8_t scl_buff[][16], uint8_t *sjis_txt, uint16_t Length, uint8_t* scl_cnt1, uint16_t* sj_cnt1);
+  void SjisToShinonomeFNTadrs(uint8_t jisH, uint8_t jisL, uint32_t* fnt_adrs);
+  void SD_Flash_ShinonomeFNTread_FHN(File ff, uint32_t addrs, uint8_t buf1[16], uint8_t buf2[16]);
+  void SD_Flash_ShinonomeFNTread_Harf_FHN(File ff, uint32_t addrs, uint8_t buf[16]);
+  uint8_t Sjis_inc_FntRead(uint8_t *sj, uint16_t length, uint16_t *sj_cnt, uint8_t buf[2][16]);
+  
+private:
+  uint8_t _cs;
+  uint32_t _SD_freq;
+  File _UtoS;
+  File _SinoZ;
+  File _SinoH;
+  ESP32_SD_UTF8toSJIS _u8ts;
+};
+
+#endif
